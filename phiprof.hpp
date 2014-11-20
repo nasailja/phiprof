bb7103c64f14cfa0774a1ca87b2c5711903ef980
Ilja Honkonen <ilja.j.honkonen@nasa.gov>
2014-11-20 10:02:07 -0500

    Fix license to real upstream one.

--- phiprof.hpp
+++ phiprof.hpp
@@ -5,20 +5,21 @@ Copyright 2010, 2011, 2012 Finnish Meteorological Institute
 Copyright 2014 Ilja Honkonen
 
 Phiprof is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3
-as published by the Free Software Foundation.
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-Phiprof is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
-Modified in 2014-11-19 from the version at
-https://github.com/fmihpc/phiprof see
-https://github.com/nasailja/phiprof for details
+
+Modified in 2014-11 from https://github.com/fmihpc/phiprof see the
+version at https://github.com/nasailja/phiprof for more info
 */
 
 #ifndef PHIPROF_HPP
